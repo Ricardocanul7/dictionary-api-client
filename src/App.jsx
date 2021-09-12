8cf7de716b8229c64febfdb17c510c675c04c06d
josue1919 <52299245+josue1919@users.noreply.github.com>
2021-09-11 22:37:55 -0500

    Create  card

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-import './App.css';
-import Button from './components/Button';
-import { useState } from 'react';
-
+import "./App.css";
+import Button from "./components/Button";
+import { useState } from "react";
+import CardInfo from "./components/Card";
 function App() {
   const [counter, setCounter] = useState(0);
 
   const handleSum = () => {
     setCounter(counter + 1);
-  }
+  };
 
   const handleRest = () => {
     setCounter(counter - 1);
-  }
+  };
 
   const handleSumByNumber = (number) => {
     setCounter(counter + number);
-  }
+  };
 
   return (
     <div className="App">
-      { counter }
+      {counter}
 
       <div>
         <Button text="-" setClick={handleRest} />
         <Button text="+" setClick={handleSum} />
         <Button text="+5" setClick={() => handleSumByNumber(5)} />
       </div>
+
+      <br />
+      <CardInfo />
     </div>
   );
 }
