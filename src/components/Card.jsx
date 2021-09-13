c09bccd39b23c5a2f2bafae33af6e0e18b1b0e25
Ricardo Canul <ricardocanul7@gmail.com>
2021-09-13 09:01:23 +0200

    Remove whispaces and format code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import './styles/Card.css'
 
-const CardInfo=()=>{
-    
-
-    return(
+const CardInfo = () => {
+    return (
         <>
-            <div className="cardInfo"> 
-                    <h4>word: Hello</h4>
-                    <p>Phonetics</p>
-                    <p>/həˈloʊ/</p>
+            <div className="cardInfo">
+                <h4>word: Hello</h4>
+                <p>Phonetics</p>
+                <p>/həˈloʊ/</p>
 
             </div>
         </>
